2b7eefcb926d53b6b97cfd420c64ff65f13988f7
gabriele92984 <60701037+gabriele92984@users.noreply.github.com>
2024-09-23 11:50:03 +0200

    Create main loop

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,13 +1,23 @@
 def bubble_sort(arr)
-   
+
   n = arr.length
 
-   for i in 0...n do
-     if arr[i - 1] > arr [i] then
-       arr[i - 1], arr[i] = arr[i], arr[i - 1]
-       p arr
-     end
-   end
+  loop do
+    swapped = false
+    for i in 0...n do
+      if arr[i - 1] > arr [i] then
+        arr[i - 1], arr[i] = arr[i], arr[i - 1] # elements swap
+        swapped = true
+
+        p arr
+      end
+    end
+
+    n -= 1
+    if swapped == false
+      break
+    end
+  end
 end
 
 bubble_sort([4,3,78,2,0,2])
